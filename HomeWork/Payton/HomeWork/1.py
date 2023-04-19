a5519f4cf8542901c9b6b7c9db369a93fa14f6b9
MrAleksoN <127983433+MrAleksoN@users.noreply.github.com>
2023-04-19 22:33:07 +0300

    Delete HomeWork/Payton/HomeWork directory

--- HomeWork/Payton/HomeWork/1.py
+++
@@ -1,7 +0,0 @@
-a = int(input("First number "))
-b = int(input("Second number "))
-c = int(input("Third number "))
-s = a+b+c
-mult = a*b*c
-print(Sum: ", s")
-print("Multiplication: ", mult)
--- HomeWork/Payton/HomeWork/3.py
+++
@@ -1,4 +0,0 @@
-d1 = float(input("First number "))
-d2 = float(input("Second diagonal "))
-s=d1*d2*0.5
-print(" Space: ", s)
