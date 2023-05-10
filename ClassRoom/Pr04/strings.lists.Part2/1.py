4209d316e94339c6d003b95d8f630303738c015e
MrAleksoN <fanat320077@gmail.com>
2023-05-10 19:24:49 +0300

    dz

--- ClassRoom/Pr04/strings.lists.Part2/1.py
+++ ClassRoom/Pr04/strings.lists.Part2/1.py
@@ -2,6 +2,6 @@ n = int(input("count of number: "))
 lst = []
 for i in range(n):
     lst.append(int(input("number["+str(i)+"] = ")))
-number = int(input) ('n =')
+number = int(input)('n =')
 count = lst(number)
 print(count) 
