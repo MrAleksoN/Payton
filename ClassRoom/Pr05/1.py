9a155eb670c3c8383aa7372f7b6bc577a5fb60db
MrAleksoN <fanat320077@gmail.com>
2023-05-10 20:28:38 +0300

    dz

---
+++ ClassRoom/Pr05/1.py
@@ -0,0 +1,8 @@
+#Напишите функцию, которая отображает на экранформатированный текст, указанный ниже:
+
+def print_text():
+    print("Don't let the noise of others opinions")
+    print('drown out your own inner voice.')
+    print("Steve Jobs")
+
+print_text()
