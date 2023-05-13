1020f3714dfc1c143ec306805da7516c70e7baa7
MrAleksoN <127983433+MrAleksoN@users.noreply.github.com>
2023-05-13 17:22:00 +0300

    Add files via upload

---
+++ HomeWork/Nested loops/3.py
@@ -0,0 +1,9 @@
+ case 'в':
+
+       for i in range(7):
+
+           for j in range(7):
+
+               if i < j and i < 7 - 1 - j:
+
+                   mat[i][j] = '*'
---
+++ HomeWork/Nested loops/7.py
@@ -0,0 +1,9 @@
+  case 'ж':
+
+       for i in range(7):
+
+            for j in range(7):
+
+                if i > j and i < 7 - 1 - j:
+
+                    mat[i][j] = '*'
