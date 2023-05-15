8f8233d3425baf1f31c7186e7f642a305236bd74
MrAleksoN <fanat320077@gmail.com>
2023-05-15 19:56:17 +0300

    dz

---
+++ ClassRoom/Pr05/Module 4 Functions. Topic Functions. Part 2/1.py
@@ -0,0 +1,11 @@
+# Напишите функцию,вычисляющию сумму элементов списка целых.Список передаётся в качестве параметра.
+
+import random
+def print_sum(n):
+    print("Введите размер списка" )
+n=int(input("k"))
+m=[7]
+for i in range(n):
+    print(random.randint(-5,17), end='')
+
+print(print_sum(n))
