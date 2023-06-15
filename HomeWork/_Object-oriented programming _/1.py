0e68d13c27380f96aa272c048b6d8277dacd7180
MrAleksoN <127983433+MrAleksoN@users.noreply.github.com>
2023-06-15 13:19:54 +0300

    Add files via upload

---
+++ HomeWork/_Object-oriented programming _/1.py
@@ -0,0 +1,37 @@
+function Machine(power) 
+  (this._enabled = false;)
+
+  (var self = this;)
+
+  this.enable = function() 
+    (self._enabled = true;)
+  
+
+  this.disable = function() 
+    (self._enabled = false;)
+  
+
+function CoffeeMachine(power) 
+  Machine.apply(this, arguments)
+
+  var waterAmount = 0
+
+  this.setWaterAmount = function(amount)
+    (waterAmount = amount;)
+  
+
+  (var parentEnable = this.enable;)
+  this.enable = function() 
+      (parentEnable(); 
+      this.run();)
+
+  function onReady() 
+    (alert( 'Кофе готово!' );)
+
+  this.run = function() 
+    (setTimeout(onReady, 1000);)
+
+
+var coffeeMachine = new CoffeeMachine(10000):
+coffeeMachine.setWaterAmount(50):
+coffeeMachine.enable():
